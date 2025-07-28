32eeffb4c92214dcc67adbc401e553785b84b4d8
chmadran <charlotte.madrangeas@gmail.com>
2025-07-28 20:35:15 +0200

    add parameters

--- benchmark/models/silero.py
+++ benchmark/models/silero.py
@@ -1,7 +1,8 @@
 from silero_vad import (load_silero_vad, get_speech_timestamps)
 import torch 
 
-# https://github.com/snakers4/silero-vad/blob/master/src/silero_vad/utils_vad.py
+#https://github.com/snakers4/silero-vad/blob/master/src/silero_vad/utils_vad.py
+#TODO: Add input sanitization
 class SileroVAD():
 
     def __init__(self, 
@@ -10,7 +11,7 @@ class SileroVAD():
                  window_size_samples : int = 512, 
                  neg_threshold: float = None,
                  visualize_probs: bool = False,
-                 time_resolution: int = 1, 
+                 time_resolution: bool = 1, 
                  return_seconds: bool = False,
                  speech_pad_ms: int = 30,
                  min_silence_duration_ms: float = 100,
@@ -29,6 +30,16 @@ class SileroVAD():
         self.threshold = threshold
         self.model = load_silero_vad()
 
+        self.window_size_samples = window_size_samples
+        self.neg_threshold = neg_threshold
+        self.visualize_probs = visualize_probs
+        self.time_resolution = time_resolution
+        self.return_seconds = return_seconds
+        self.speech_pad_ms = speech_pad_ms
+        self.min_silence_duration_ms = min_silence_duration_ms
+        self.max_speech_duration_s = max_speech_duration_s
+        self.min_speech_duration_ms = min_speech_duration_ms
+
     def get_confidence(self, wav):
         predict = self.model.audio_forward(wav, sr=self.sampling_rate)
         return predict
@@ -39,6 +50,14 @@ class SileroVAD():
             self.model,
             sampling_rate=self.sampling_rate,
             threshold=self.threshold,
+            window_size_samples= self.window_size_samples,
+            neg_threshold = self.neg_threshold, 
+            visualize_probs = self.visualize_probs,
+            return_seconds = self.return_seconds,
+            speech_pad_ms = self.speech_pad_ms,
+            min_silence_duration_ms = self.min_silence_duration_ms,
+            max_speech_duration_s = self.max_speech_duration_s,
+            min_speech_duration_ms = self.min_speech_duration_ms
         )
         return timestamps
 
